e9d1930e26468c0d8cd4946138b8a712e682fb41
Chris Doherty <chris.doherty4@gmail.com>
2017-10-28 20:32:08 +0100

    Adjusted licensing to include full name

--- src/Backup/Commands/FtpPush.php
+++ src/Backup/Commands/FtpPush.php
@@ -1,6 +1,6 @@
 <?php
 /* 
- * Copyright (C) 2017 chrisdoherty
+ * Copyright (C) 2017 Chris Doherty
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
